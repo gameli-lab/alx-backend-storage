0e5069490974101ab3abcecc49998adba0cdcf33
Benjamin Torfu <btorfu@proton.me>
2024-08-12 20:24:01 +0000

    mongo py all

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -7,5 +7,17 @@ from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
-    ''' Lists all collections in the databases '''
-    if not 
+    '''
+    Lists all collections in a Mongodb collection
+
+    Parameters:
+    mongo_collection (pymongo.collection.Collection): The collection object to
+    query.
+
+    Returns:
+    list: A list of documents in the collection. Returns an empty list if no
+    documents exist.
+    '''
+    docs = list(mongo_collection.find())
+
+    return docs
