0e0f994049d629350cb1f801cbb9f0b2e581acfa
Benjamin Torfu <btorfu@proton.me>
2024-08-12 15:13:19 +0000

    mongo shell

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+This module lists all documents ina collection
+'''
+
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+    ''' Lists all collections in the databases '''
+    if not 
