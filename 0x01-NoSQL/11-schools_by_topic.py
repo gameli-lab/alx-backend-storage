8c8a6c9934bf6e9917e11a02e617aff2b8e9cfef
Benjamin Torfu <btorfu@proton.me>
2024-08-13 09:35:57 +0000

    mongo py find

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''
+This module returns the list of school having a specific topic
+'''
+
+from pymongo import MongoClient
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''
+    Lists schools by topic
+
+    Parameters:
+    mongo_collection: a mongo collection object
+
+    topic: topic to display
+
+    Returns:
+    A list of schools with specific topics
+    '''
+    schools = mongo_collection.find({"topic": topic})
+    return list(schools)
